3296f84d7ce1c977387cfe7f74635eb0b11bb277
Aulus Diniz <aulus999@gmail.com>
2015-06-15 21:16:40 -0300

    making comments auto appear.

--- app/public/js/views/vote.js
+++ app/public/js/views/vote.js
@@ -8,6 +8,7 @@ function commentAndVote(vote) {
     },
     success	: function(responseText, status, xhr, $form){
       console.log('comentário publicado com sucesso!');
+      location.reload();
     },
     error : function(e){
       console.log('publicação de comentário falhou : ' + e.responseText );
