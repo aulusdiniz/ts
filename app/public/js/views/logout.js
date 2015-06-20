1748b70337eb31103367caac4fddfea4b96e55bc
Aulus Diniz <aulus999@gmail.com>
2015-06-19 21:03:32 -0300

    pdsfasdf

--- app/public/js/views/logout.js
+++ app/public/js/views/logout.js
@@ -10,12 +10,3 @@ function deleteCookies()
       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
   }
 }
-
-setInterval(function(){
-  deleteCookies();
-}, 5000);
-
-function checkLogin()
-{
-
-}
